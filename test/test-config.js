a4bbe14da376249104ad022e8747e8d474d3d689
David Sato <david.sato64@gmail.com>
2017-08-02 11:50:16 -0700

    updated jsdom to 11.0 syntax, added enzyme, jsdom and sinon as packages and ran successful test - removed old package.json and tweaked package.json to better reflect repo

--- test/test-config.js
+++ test/test-config.js
@@ -1,3 +1,9 @@
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const { document } = (new JSDOM('')).window;
+
+const exposedProperties = ['window', 'navigator', 'document'];
+
 // -------------------------------
 // Disable webpack-specific features for tests since
 // Mocha doesn't know what to do with them.
@@ -5,3 +11,16 @@
 ['.css', '.scss', '.png', '.jpg'].forEach(ext => {
     require.extensions[ext] = () => null;
 });
+
+global.document = document;
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+    if (typeof global[property] === 'undefined') {
+        exposedProperties.push(property);
+        global[property] = document.defaultView[property];
+    }
+});
+
+global.navigator = {
+    userAgent: 'node.js'
+};
