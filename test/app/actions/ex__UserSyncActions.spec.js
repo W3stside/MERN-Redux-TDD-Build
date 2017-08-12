5d04c31dfbbd3ee5c0f8bf5f218ebe8327b0d820
David Sato <david.sato64@gmail.com>
2017-08-12 15:13:05 -0700

    documentation for syncAction unit test

--- test/app/actions/ex__UserSyncActions.spec.js
+++ test/app/actions/ex__UserSyncActions.spec.js
@@ -5,10 +5,18 @@ import {expect} from 'chai';
 import * as actions from '../../../app/actions';
 
 /** EXAMPLE React Redux Sync Actions Unit Test
-**********************************************
-**/
-
+ * How to test Action Creators
+ * @type {function}
+ * @param {string} string Top level description of entire test
+ * @param {function} function Fires first it() unit test
+ */
 describe('actions', () => {
+    /**
+     * Individual tests within describe block
+     * @type {function}
+     * @param {string} string Specific description of unit test
+     * @param {function} function Fires actual test
+     */
     it('TEXT_INPUT action should add Input', () => {
         const input = 'poop';
         const field = 'NAME';
