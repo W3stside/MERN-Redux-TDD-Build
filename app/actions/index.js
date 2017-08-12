6d980c0aec024ae1fa00d517adb791471a2bebfb
David Sato <david.sato64@gmail.com>
2017-08-12 12:03:07 -0700

    documented handleTextInput actionCreator

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,14 +1,10 @@
 import * as types from './types';
 
-/** EXAMPLE ACTION:
-export function filterTable(filter) {
-    return {
-        type: types.FILTER,
-        filter
-    };
-}
-*/
-
+/**
+ * Redux actionCreator that fires when handling text inputs (input fields, forms etc)
+ * @param  {string} input event.target.value from input nodes//components
+ * @return {object}       object with TYPE and PAYLOAD for Reducer to handle
+ */
 export const handleTextInput = input => field => {
     return {
         type: types.TEXT_INPUT,
