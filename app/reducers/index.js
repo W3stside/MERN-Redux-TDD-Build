a0eae965898bbe2bc834e5d3fa082e0f063f86d6
David Sato <david.sato64@gmail.com>
2017-08-12 12:12:21 -0700

    docs

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,11 @@
 import { routerReducer as routing } from 'react-router-redux';
 import { combineReducers } from 'redux';
 
+/**
+ * Redux root Reducer - exports imported Reducers with each KEY as a REDUCER
+ * IMPORTANT: import each separate Reducer with the same NAME as the slice of STATE it handles
+ * @type {object}
+ */
 const rootReducer = combineReducers({
     routing
 });
